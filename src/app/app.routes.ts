654580cd0241e72312a75fdf72ab3cc9b20bde0c
Domiidp04 <domiddp2004@gmail.com>
2024-03-28 19:22:06 +0100

    tonterias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: LandingComponent },
+  { path: '**', component: NotFoundComponent }
+];
