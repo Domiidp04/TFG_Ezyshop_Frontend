f6494f3215f41b7adaa5d7d4422bfcbc63dfe794
Domiidp04 <domiddp2004@gmail.com>
2024-04-02 19:48:21 +0200

    Refactorizado y un poco mejorado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, MenubarModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'File',
+      },
+      {
+        label: 'Edit',
+        items: [
+          {
+            label: 'Left',
+            icon: 'pi pi-fw pi-align-left',
+            url: '/edit?layout=1',
+          },
+          {
+            label: 'Right',
+            icon: 'pi pi-fw pi-align-right',
+          },
+          {
+            label: 'Center',
+            icon: 'pi pi-fw pi-align-center',
+          },
+          {
+            label: 'Justify',
+            icon: 'pi pi-fw pi-align-justify',
+          },
+        ],
+      },
+      {
+        label: 'Users',
+      },
+      {
+        label: 'Events'
+      },
+      {
+        label: 'Quit',
+      },
+    ];
+  }
 }
