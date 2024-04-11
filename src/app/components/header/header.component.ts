a5a7a736df4c0102f76b7118f6556f111868b866
Domiidp04 <domiddp2004@gmail.com>
2024-04-11 14:46:33 +0200

    Algo subido

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem, PrimeIcons } from 'primeng/api';
+import { MENU_ITEMS } from '../../../assets/menu-items';
 
 @Component({
   selector: 'app-header',
@@ -13,41 +14,6 @@ import { MenuItem, PrimeIcons } from 'primeng/api';
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
   ngOnInit(): void {
-    this.items = [
-      {
-        label: 'File',
-      },
-      {
-        label: 'Edit',
-        items: [
-          {
-            label: 'Left',
-            icon: 'pi pi-fw pi-align-left',
-            url: '/edit?layout=1',
-          },
-          {
-            label: 'Right',
-            icon: 'pi pi-fw pi-align-right',
-          },
-          {
-            label: 'Center',
-            icon: 'pi pi-fw pi-align-center',
-          },
-          {
-            label: 'Justify',
-            icon: 'pi pi-fw pi-align-justify',
-          },
-        ],
-      },
-      {
-        label: 'Users',
-      },
-      {
-        label: 'Events'
-      },
-      {
-        label: 'Quit',
-      },
-    ];
+    this.items = MENU_ITEMS;
   }
 }
---
+++ src/assets/menu-items.ts
@@ -0,0 +1,39 @@
+export const MENU_ITEMS =  [
+  {
+    label: 'Novedades',
+    url: '/novedades',
+  },
+  {
+    label: 'Coleccion',
+    items: [
+      {
+        label: 'Sudaderas',
+        url: '/edit?layout=1',
+      },
+      {
+        label: 'Camisetas',
+        url: '/edit?layout=1'
+      },
+      {
+        label: 'Pantalones',
+        url: '/edit?layout=1'
+      },
+      {
+        label: 'Camisas'
+      },
+    ],
+  },
+  {
+    label: 'Calzado',
+    url: '/calzados'
+  },
+  {
+    label: 'Accesorios',
+    url: '/accesorios'
+  },
+  {
+    label: 'Editorial',
+    url: '/editorial',
+  },
+];
+
