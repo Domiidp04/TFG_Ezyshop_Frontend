9ab74decca7d69692a65dc1aef95848a2f9cf1e2
Domiidp04 <domiddp2004@gmail.com>
2024-04-11 22:07:21 +0200

    Realizando mejoras...

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,17 +3,34 @@ import { LogoComponent } from '../logo/logo.component';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem, PrimeIcons } from 'primeng/api';
 import { MENU_ITEMS } from '../../../assets/menu-items';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponent, MenubarModule],
+  imports: [LogoComponent, MenubarModule, OverlayPanelModule, SidebarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
+  isCarrito: boolean = false;
+
   ngOnInit(): void {
     this.items = MENU_ITEMS;
   }
+
+  switchCarrito(){
+    if(this.isCarrito){
+      this.isCarrito=false;
+    }else{
+      this.isCarrito=true;
+    }
+  }
+
+
+
 }
--- src/assets/menu-items.ts
+++ src/assets/menu-items.ts
@@ -4,32 +4,8 @@ export const MENU_ITEMS =  [
     url: '/novedades',
   },
   {
-    label: 'Coleccion',
-    items: [
-      {
-        label: 'Sudaderas',
-        url: '/edit?layout=1',
-      },
-      {
-        label: 'Camisetas',
-        url: '/edit?layout=1'
-      },
-      {
-        label: 'Pantalones',
-        url: '/edit?layout=1'
-      },
-      {
-        label: 'Camisas'
-      },
-    ],
-  },
-  {
-    label: 'Calzado',
-    url: '/calzados'
-  },
-  {
-    label: 'Accesorios',
-    url: '/accesorios'
+    label: 'Categorias',
+    url: '/categorias'
   },
   {
     label: 'Editorial',
